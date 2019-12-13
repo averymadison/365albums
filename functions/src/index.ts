cf977d3ce229e66a59a6273618c75a091d67a7fe
Kyle Decker <kylebdecker@gmail.com>
2019-12-13 02:29:57 -0500

    Typecheck

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,11 @@
 import * as functions from 'firebase-functions';
 import * as cors from 'cors';
 import * as express from 'express';
-import { Request, Response } from 'express';
 
 const app = express();
 
 app.use(cors({ origin: true }));
-app.get('*', (req: Request, res: Response) => {
+app.get('*', (req: express.Request, res: express.Response) => {
   res.send('Hello from express on Firebase with CORS!');
 });
 
