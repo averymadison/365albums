1823f8cd7d358b8728a0fc39c20af09478725ab4
Kyle Decker <kyle.decker@cityblock.com>
2019-12-24 20:38:20 -0500

    Dont' use grid

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
 
 const Landing = () => (
   <div>
-    <h1>Landing</h1>
+    <h1>365albums</h1>
+    <p>Listen to an album a day.</p>
+    <Link to={ROUTES.SIGN_UP} className="button">
+      Sign Up
+    </Link>
+    <Link to={ROUTES.SIGN_IN} className="button">
+      Sign In
+    </Link>
   </div>
 );
 
