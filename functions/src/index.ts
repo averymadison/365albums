9d73e834b718c09c6495635b09898ace24261906
Kyle Decker <kyle.decker@cityblock.com>
2020-01-16 13:24:59 -0500

    Typescript types

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const cors = require("cors")({ origin: true });
 const bandcamp = require("bandcamp-scraper");
-const spotifyWebApi = require("spotify-web-api-node");
+import spotifyWebApi from "spotify-web-api-node";
 const discogsWebApi = require("disconnect").Client;
 
 const spotify = new spotifyWebApi({
@@ -14,18 +14,18 @@ const discogs = new discogsWebApi({
   consumerSecret: functions.config().discogs.secret
 }).database();
 
-spotify.clientCredentialsGrant().then(
-  function(data: any) {
+spotify
+  .clientCredentialsGrant()
+  .then(data => {
     console.log("The access token expires in " + data.body["expires_in"]);
     console.log("The access token is " + data.body["access_token"]);
 
     // Save the access token so that it's used in future calls
     spotify.setAccessToken(data.body["access_token"]);
-  },
-  function(err: any) {
+  })
+  .catch(err => {
     console.log("Something went wrong when retrieving an access token", err);
-  }
-);
+  });
 
 exports.search = functions.https.onRequest((req: any, res: any) => {
   const type = req.query.type;
